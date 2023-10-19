60ebd532454ce70813232f8030ba0c35ebc62b8f
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-10-18 22:32:13 -0500

    brain has clearly stopped working

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,26 +1,27 @@
-int whoWins(Map<String, int> teamA, Map<String, int> teamB);
-num getPoints(Map team) {
-  num points = 0;
-  team.forEach((key, value) {
-    if (key == 'Free throws') {
-      points += value;
-    } else if (key == '2 pointer') {
-      points += value * 2;
-    } else if (key == '3 pointer') {
-      points += value * 3;
-    }
-  });
-  return points;
-}
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  num getPoints(Map team) {
+    num points = 0;
+    team.forEach((key, value) {
+      if (key == 'Free throws') {
+        points += value;
+      } else if (key == '2 pointer') {
+        points += value * 2;
+      } else if (key == '3 pointer') {
+        points += value * 3;
+      }
+    });
+    return points;
+  }
 
-num whoWins(Map teamA, Map teamB) {
-  num teamAPoints = getPoints(teamA);
-  num teamBPoints = getPoints(teamB);
-  if (teamAPoints > teamBPoints) {
-    return 1;
-  } else if (teamAPoints < teamBPoints) {
-    return 2;
-  } else {
-    return 0;
+  num whoWins(Map teamA, Map teamB) {
+    num teamAPoints = getPoints(teamA);
+    num teamBPoints = getPoints(teamB);
+    if (teamAPoints > teamBPoints) {
+      return 1;
+    } else if (teamAPoints < teamBPoints) {
+      return 2;
+    } else {
+      return 0;
+    }
   }
 }
