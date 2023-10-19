72c33a1fde7d1cc50303274b8116ac207b444c62
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-10-18 22:39:08 -0500

    please work now

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,27 +1,20 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-  num getPoints(Map team) {
-    num points = 0;
-    team.forEach((key, value) {
-      if (key == 'Free throws') {
-        points += value;
-      } else if (key == '2 pointer') {
-        points += value * 2;
-      } else if (key == '3 pointer') {
-        points += value * 3;
-      }
-    });
-    return points;
-  }
+  int teamA_points = 0;
+  int teamB_points = 0;
 
-  num whoWins(Map teamA, Map teamB) {
-    num teamAPoints = getPoints(teamA);
-    num teamBPoints = getPoints(teamB);
-    if (teamAPoints > teamBPoints) {
-      return 1;
-    } else if (teamAPoints < teamBPoints) {
-      return 2;
-    } else {
-      return 0;
-    }
+  teamA_points += teamA['Free throws'] ?? 0;
+  teamA_points += (teamA['2 pointers'] ?? 0) * 2;
+  teamA_points += (teamA['3 pointers'] ?? 0) * 3;
+
+  teamB_points += teamB['Free throws'] ?? 0;
+  teamB_points += (teamB['2 pointers'] ?? 0) * 2;
+  teamB_points += (teamB['3 pointers'] ?? 0) * 3;
+
+  if (teamA_points > teamB_points) {
+    return 1;
+  } else if (teamA_points < teamB_points) {
+    return 2;
+  } else {
+    return 0;
   }
 }
