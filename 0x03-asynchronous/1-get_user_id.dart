4e724b05a4d2ecdc0681209cfad8440e7a79ff45
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2023-10-18 22:53:33 -0500

    testing different method

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,15 +1,10 @@
-import 'dart:convert';
+import '0-util.dart';
 
-Future<String> fetchUserData() => Future.delayed(
-  const Duration(seconds: 2),
-      () =>
-  '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-);
-
-getUserId() async {
-  var strRepr = await fetchUserData();
-
-  var Repr  = json.decode(strRepr);
-
-  return Repr["id"];
+Future<void> usersCount() async {
+  try {
+    final userCount = await fetchUsersCount();
+    print('$userCount');
+  } catch (error) {
+    print('Failed to fetch user count: $error');
+  }
 }
